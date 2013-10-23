806a5fa6ae91a2d25ecc24ead8a8e62b31ca278b
Gabriel Paquin <gpaquin@webagecorp.com>
2013-10-23 20:21:06 +0800

    created recipes folder

--- deploy/recipes/extra.rb
+++
@@ -1,7 +0,0 @@
-namespace :extra do
-  desc "Install extra packages."
-  task :install, roles: :web do
-    run "#{sudo} apt-get install libxml2-dev libxslt1-dev imagemagick"
-  end
-  after "deploy:install", "extra:install"
-end
--- deploy/recipes/rake.rb
+++
@@ -1,5 +0,0 @@
-namespace :run_rake do
-  task :invoke do
-    run "cd #{current_path} && RAILS_ENV=production rake #{ENV['COMMAND']}"
-  end
-end
